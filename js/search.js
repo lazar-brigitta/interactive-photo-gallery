a236478c599e386bdee75d44f98e18ac7857f314
lazar-brigitta <lazar.brigitta@yahoo.com>
2022-10-25 20:30:34 +0300

    update js

--- js/search.js
+++ js/search.js
@@ -1,11 +1,11 @@
 let input = document.getElementById("search").value;
 input=input.toLowerCase();
 let y = input.addEventListener("keyup", search());
-let x = document.querySelectorAll(".photo a");
+let x = document.querySelectorAll(".photo a").getAttribute("data-caption");
 
 function search() {
     for(i = 0; i < x.length; i++) {
-        if(!x[i].getAttribute("data-caption").toLowerCase().includes(input)) {
+        if(!x[i].toLowerCase().includes(input)) {
            return x[i].style.display="none"; 
         } else {
             return x[i].style.display="a";
